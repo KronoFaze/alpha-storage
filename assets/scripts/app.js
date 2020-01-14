3a33a22270d3972c73a845368daae1dfcf9edc2c
KronoFaze <marifurrahmanrabbi@gmail.com>
2020-01-14 23:19:46 +0600

    added sign up form and toggle feature

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,16 @@
-document.getElementById('loginButton').addEventListener("onmouseover", (evt) => {
-	evt.target.
-})
+const formSignInToggleButton = document.querySelector(".form-sign-in-toggle-button");
+const formSignUpToggleButton = document.querySelector(".form-sign-up-toggle-button");
+
+formSignInToggleButton.addEventListener("click", function() {
+	document.querySelector(".sign-in-form").style.display = "none";
+	document.querySelector(".sign-up-form").style.display = "block";
+	document.querySelector(".sign-in-form").classList.remove("form-toggle");
+	document.querySelector(".sign-up-form").classList.add("form-toggle");
+});
+
+formSignUpToggleButton.addEventListener("click", function() {
+	document.querySelector(".sign-up-form").style.display = "none";
+	document.querySelector(".sign-in-form").style.display = "block";
+	document.querySelector(".sign-up-form").classList.remove("form-toggle");
+	document.querySelector(".sign-in-form").classList.add("form-toggle");
+});
