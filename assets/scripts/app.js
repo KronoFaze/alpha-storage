67adeac65228fad643d4ed5435e8808b08ec038a
KronoFaze <marifurrahmanrabbi@gmail.com>
2020-01-19 18:47:49 +0600

    redesigned the whole website

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,9 @@
-const formSignInToggleButton = document.querySelector(".form-sign-in-toggle-button");
-const formSignUpToggleButton = document.querySelector(".form-sign-up-toggle-button");
-
-formSignInToggleButton.addEventListener("click", function() {
-	document.querySelector(".sign-in-form").classList.remove("fade-in");
-	document.querySelector(".sign-in-form").style.display = "none";
-	document.querySelector(".sign-up-form").classList.add("fade-in");
-	document.querySelector(".sign-up-form").style.display = "block";
-});
-
-formSignUpToggleButton.addEventListener("click", function() {
-	document.querySelector(".sign-up-form").classList.remove("fade-in");
-	document.querySelector(".sign-up-form").style.display = "none";
-	document.querySelector(".sign-in-form").classList.add("fade-in");
-	document.querySelector(".sign-in-form").style.display = "block";
+$(function() {
+	$(".ham-menu").on("click", function() {
+		if ($(".collapse").hasClass("active")) {
+			$(".collapse").removeClass("active");
+		} else {
+			$(".collapse").addClass("active");
+		}
+	});
 });
