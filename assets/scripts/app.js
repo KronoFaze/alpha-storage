d1d95e427e88757569d136957183055f104ed7ee
KronoFaze <marifurrahmanrabbi@gmail.com>
2020-01-25 22:07:48 +0600

    added scroll effect on navbar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,10 @@
-$(function() {
-	$(".ham-menu").on("click", function() {
-		console.log($(".collapse").height());
-	});
-});
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollPos > currentScrollPos || currentScrollPos < 100) {
+		document.querySelector("nav").style.top = "0";
+	} else {
+		document.querySelector("nav").style.top = "-10rem";
+	}
+	prevScrollPos = currentScrollPos;
+};
